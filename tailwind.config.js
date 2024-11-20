364a6bf23b5a70e6409b560e27963f262ad6d4c9
Noland <88490545+NolandDotU@users.noreply.github.com>
2024-11-20 13:50:46 +0700

    start

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.html",
+  ],
+  theme: {
+    fontFamily: {
+      'sans': ['"Poppins"', 'sans-serif'],
+      'nunito': ['"Nunito Sans"', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
