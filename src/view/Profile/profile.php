a415c423dda3645692686252f9e68e2654be419e
Noland <88490545+NolandDotU@users.noreply.github.com>
2024-11-26 15:37:29 +0700

    tambah Font HK Grostek

--- src/view/Profile/profile.php
+++ src/view/Profile/profile.php
@@ -13,7 +13,12 @@
     href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap"
     rel="stylesheet" />
 
-  <title>Notifikasi</title>
+  <!-- font Host Grotesk -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Host+Grotesk:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+
+  <title>Profile</title>
 </head>
 
 <body class="overflow-scroll">
@@ -21,7 +26,7 @@
 
   <!-- Nav -->
   <header
-    class="flex bg-primary w-[375px] justify-center items-center">
+    class="flex bg-primary w-full justify-center items-center">
     <h6
       class="text-left font-nunito font-semibold text-lg pl-5 pb-2 textred text-white w-full justify-center">
       Profile
@@ -33,24 +38,31 @@
   <!-- Profile & General -->
   <section>
     <div class="flex pl-[18px] pt-6 pb-6">
-      <img src="/assets/sorry.jpg" class="w-14 h-14 object-cover rounded-full" alt="">
+      <img src="/src/assets/sorry.jpg" class="w-14 h-14 object-cover rounded-full" alt="">
       <div class="pl-[18px]">
-        <h6 class="font-nunito font-semibold text-lg text-[#33333] tracking-tight">Deandra Emilya</h6>
+        <h6 class="font-nunito font-semibold text-lg text-[#333333] tracking-tight">Deandra Emilya</h6>
         <p class="text-sm font-medium font-nunito tracking-tight">yellowclaw@gmail.com</p>
       </div>
     </div>
 
     <!-- Saldo dan Poin  -->
-    <div class="mx-auto w-[339px] h-[68px] bg-white rounded-xl shadow border border-[#f2f2f2] flex items-center">
+    <div class="w-[339px] h-[68px] mx-auto bg-white rounded-xl shadow border border-[#f2f2f2]">
       <!-- saldo tabungan -->
       <div class="flex">
-        <img src="/assets/Wallet.png" class="pl-[15px] pt-[19px]" alt="">
+        <img src="/src/assets/WalletICon.svg" class="pl-[15px] pt-[19px]" alt="">
         <div class="pl-[14px] pt-[18px]">
           <p class="text-[#828282] text-[10px] font-medium font-nunito tracking-tight">Saldo Tabungan</p>
           <h6 class="text-[#333333] text-sm font-semibold font-nunito tracking-tight">Rp 1.500.000</h6>
         </div>
+        <img src="/src/assets/Vector 10.svg" class="pl-[31px] pt-[12px]" alt="">
+
+        <!-- Poin Tabungan -->
+        <img src="/src/assets/Star.svg" class="pl-[25px] pt-[19px]" alt="">
+        <div class="pl-[14px] pt-[18px]">
+          <p class="text-[#828282] text-[10px] font-medium font-nunito tracking-tight">Poin Tabungan</p>
+          <h6 class="text-[#333333] text-sm font-semibold font-nunito tracking-tight">150 <span class="text-[#828282] text-xs font-normal font-HK tracking-tight">Poin</span></h6>
+        </div>
       </div>
-      <img src="/assets/Vector 10.svg" class="ml-auto pr-[15px]" alt="">
     </div>
   </section>
 
