8048171f0f4655c279fd6336af0442e8694626a2
Noland <88490545+NolandDotU@users.noreply.github.com>
2024-11-26 17:25:12 +0700

    PROFILE bagian navbar bawah

--- src/view/Profile/profile.php
+++ src/view/Profile/profile.php
@@ -28,15 +28,15 @@
   <header
     class="flex bg-primary w-full justify-center items-center">
     <h6
-      class="text-left font-nunito font-semibold text-lg pl-5 pb-2 textred text-white w-full justify-center">
+      class="flex justify-between text-left font-nunito font-semibold text-lg pl-5 pb-2 textred text-white w-full justify-center">
       Profile
+      <img src="/src/assets/uil_edit.png" class="pr-[18px]" alt="">
     </h6>
-
-    <img src="/assets/uil_edit.png" class="pb-2 pr-5" alt="">
   </header>
 
   <!-- Profile & General -->
   <section>
+    <!-- Profile -->
     <div class="flex pl-[18px] pt-6 pb-6">
       <img src="/src/assets/sorry.jpg" class="w-14 h-14 object-cover rounded-full" alt="">
       <div class="pl-[18px]">
@@ -54,7 +54,7 @@
           <p class="text-[#828282] text-[10px] font-medium font-nunito tracking-tight">Saldo Tabungan</p>
           <h6 class="text-[#333333] text-sm font-semibold font-nunito tracking-tight">Rp 1.500.000</h6>
         </div>
-        <img src="/src/assets/Vector 10.svg" class="pl-[31px] pt-[12px]" alt="">
+        <img src="/src/assets/Vector 10.svg" class="pl-[31px] pt-[12px] pb-[12px]" alt="">
 
         <!-- Poin Tabungan -->
         <img src="/src/assets/Star.svg" class="pl-[25px] pt-[20px] pb-[20px]" alt="">
@@ -63,6 +63,119 @@
           <h6 class="text-[#333333] text-sm font-semibold font-nunito tracking-tight">150 <span class="text-[#828282] text-xs font-normal font-HK-Grostek tracking-tight">Poin</span></h6>
         </div>
       </div>
+    </div>
+
+    <!-- Umum -->
+    <div class="pt-[24px] pl-[18px]">
+      <h2 class="w-[147px] h-6 text-[#333333] text-base font-semibold font-nunito tracking-tight">Umum</h2>
+
+      <!-- tombol Ubah nama & Email -->
+      <a href="">
+        <h3 class="flex justify-between text-[#4f4f4f] text-sm font-medium font-nunito tracking-tight pt-[18px] pb-[18px]">
+          Ubah Nama & Email
+          <img src="/src/assets/chevron-right.svg" class="self-center pr-[18px]" alt="">
+        </h3>
+      </a>
+      <div class="w-[343px] bg-[#F6F7FA] h-[1px]"></div>
+
+      <!-- tombol Ubah kata sandi -->
+      <a href="">
+        <h3 class="flex justify-between text-[#4f4f4f] text-sm font-medium font-nunito tracking-tight pt-[18px] pb-[18px]">
+          Ubah Kata Sandi
+          <img src="/src/assets/chevron-right.svg" class="self-center pr-[18px]" alt="">
+        </h3>
+      </a>
+    </div>
+
+
+    <div class="w-full bg-[#F6F7FA] h-[8px]"></div>
+
+    <!-- Tentang -->
+    <div class="pt-[24px] pl-[18px]">
+      <h2 class="w-[147px] h-6 text-[#333333] text-base font-semibold font-nunito tracking-tight">Tentang</h2>
+
+      <!-- Tombol Tentang Desa Digital -->
+      <a href="">
+        <h3 class="flex justify-between text-[#4f4f4f] text-sm font-medium font-nunito tracking-tight pt-[18px] pb-[18px]">
+          Tentang Desa Digital
+          <img src="/src/assets/chevron-right.svg" class="self-center pr-[18px]" alt="">
+        </h3>
+      </a>
+
+      <div class="w-[343px] bg-[#F6F7FA] h-[1px]"></div>
+
+      <!-- tombol FAQ -->
+      <a href="">
+        <h3 class="flex justify-between text-[#4f4f4f] text-sm font-medium font-nunito tracking-tight pt-[18px] pb-[18px]">
+          FAQ
+          <img src="/src/assets/chevron-right.svg" class="self-center pr-[18px]" alt="">
+        </h3>
+      </a>
+
+      <div class="w-[343px] bg-[#F6F7FA] h-[1px]"></div>
+
+      <!-- tombol Syarat & Ketentuan -->
+      <a href="">
+        <h3 class="flex justify-between text-[#4f4f4f] text-sm font-medium font-nunito tracking-tight pt-[18px] pb-[18px]">
+          Syarat & Ketentuan
+          <img src="/src/assets/chevron-right.svg" class="self-center pr-[18px]" alt="">
+        </h3>
+      </a>
+    </div>
+
+    <div class="w-full bg-[#F6F7FA] h-[8px]"></div>
+
+    <div class="pt-[24px] pb-[25px] pl-[18px]">
+      <!-- Tombol Keluar -->
+      <a href="">
+        <h3 class="flex justify-between text-[#4f4f4f] text-sm font-medium font-nunito tracking-tight pt-[24px] pb-[18px]">
+          Keluar
+          <img src="/src/assets/chevron-right.svg" class="self-center pr-[18px]" alt="">
+        </h3>
+      </a>
+    </div>
+    </div>
+
+
+    <!-- Kolom APP VERSION -->
+    <div class="w-[375px] h-[63px] bg-[#f5f6f9] justify-center items-center flex">
+      <p class="text-[#828282] text-xs font-normal font-HK-Grostek tracking-tight">Versi 1.01</p>
+    </div>
+
+  </section>
+
+
+  <!-- Navbar Bawah -->
+  <section>
+    <div class="w-[375px] h-[83px] bg-white flex justify-around items-center">
+
+      <!-- HOME -->
+      <div class="flex flex-col items-center">
+        <img src="/src/assets/Nav Item/24/basic/home.svg" alt="Home">
+        <p class="text-[#bdbdbd] text-[10px] font-semibold font-nunito tracking-tight">Home</p>
+      </div>
+
+      <!-- AKTIVITAS -->
+      <div class="flex flex-col items-center">
+        <img src="/src/assets/Nav Item/uil_invoice.svg" alt="Aktivitas">
+        <p class="text-[#bdbdbd] text-[10px] font-semibold font-nunito tracking-tight">Aktivitas</p>
+      </div>
+
+      <!-- SCAN QR -->
+       <div class="absolute w-[70px] h-[70px] bg-[#fc8210] rounded-full shadow"></div>
+
+      <!-- NOTIFIKASI -->
+      <div class="flex flex-col items-center">
+        <img src="/src/assets/Nav Item/uil_bell.svg" alt="Notifikasi">
+        <p class="text-[#bdbdbd] text-[10px] font-semibold font-nunito tracking-tight">Notifikasi</p>
+      </div>
+
+      <!-- PROFIL -->
+      <div class="flex flex-col items-center">
+        <img src="/src/assets/Nav Item/24/basic/user.svg" alt="Profil">
+        <p class="text-primary text-[10px] font-semibold font-nunito tracking-tight">Profil</p>
+      </div>
+
     </div>
   </section>
 
